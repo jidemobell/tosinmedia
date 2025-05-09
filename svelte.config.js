f157b90e9b321bfdd9957881d1efeb25e93f9311
Olajide Olaniyan <jidd32@gmail.com>
2025-05-09 16:15:02 +0100

    put back flare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,10 @@
-// import adapter from '@sveltejs/adapter-cloudflare';
-import vercel from '@sveltejs/adapter-vercel';
+import adapter from '@sveltejs/adapter-cloudflare';
+// import vercel from '@sveltejs/adapter-vercel';
 
 
 // config for cloudflare
 export default {
   kit: {
-    adapter: vercel()
+    adapter: adapter()
   }
 };
