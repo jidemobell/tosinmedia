98bee8a8d178d3948dc154552a2d235a5c13e83e
Olajide Olaniyan <jidd32@gmail.com>
2025-05-09 15:58:21 +0100

    add vercel config

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,10 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+// import adapter from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
+
 
 // config for cloudflare
 export default {
   kit: {
-    adapter: adapter()
+    adapter: vercel()
   }
 };
